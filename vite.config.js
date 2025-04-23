a94c0210b89589782b8c49ccac607ead0a482306
eng-ahmad-allawi <your-email@example.com>
2025-04-23 18:10:15 +0300

    Prepare for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gaming-hall-app/', // اسم المستودع الذي ستستخدمه على GitHub
   server: {
     port: 3000,
     open: true
